a94a4ceb101843ff5d00d4cbf3f0ccbdca09efce
salmansheri <sheriffsalman00@gmail.com>
2026-03-28 09:59:33 +0530

    initial commit

---
+++ ChatAppBackend/src/test/java/com/salman/ChatAppBackend/ChatAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salman.ChatAppBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
